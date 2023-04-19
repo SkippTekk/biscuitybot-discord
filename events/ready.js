1aaedf025f37fe922d5f8ad69724b55270b0f1d9
Alek Josef Balash (SkippTekk) <skipptekk@gmail.com>
2023-04-18 20:31:54 -0600

    changed ready status to guild/user numbers

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is loaded`);
-        client.user.setActivity(`${client.guilds.cache.size}`, { type: ActivityType.Watching });
+        client.user.setActivity(`${client.guilds.cache.size} guilds with ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users`, { type: ActivityType.Watching });
         client.user.setStatus('online');
     }
 }
