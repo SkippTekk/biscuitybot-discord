caf7d5f6ea05dbca9f73319ba41c068c0aabb193
SkippTekk <skipptekk@gmail.com>
2023-04-24 13:01:45 -0600

    upload for assistance

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,26 +1,55 @@
-const { Collection } = require('discord.js');
+const { Collection, PermissionsBitField, ChannelType } = require('discord.js');
 const fs = require('fs');
 const Guild = require('../models/guild')
+const Ticket = require('../models/ticket')
 
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
         const dbGuild = await Guild.findOne({ where: { id: interaction.guild.id } });
+        const dbTicket = await Ticket.findOne({ where: { id: interaction.guild.id } });
         if (interaction.isChatInputCommand()) {
 
             let command = client.commands.get(interaction.commandName);
 
-            try {
-                if (interaction.replied) return;
-                command.execute(interaction);
-            } catch (error) {
-                console.log(error);
-                interaction.reply({ content: 'Sorry, command isn\'t working yet.', ephemeral: true })
+            if (interaction.replied) {
+                return command.execute(interaction);
             }
         } else if (interaction.isButton()) {
             if (interaction.customId.includes('verify')) {
                 return [interaction.member.roles.add(dbGuild.verifyRole), await interaction.member.roles.remove(dbGuild.defaultRole)]
+            } else if (interaction.customId.includes('create')) {
+                return [interaction.guild.channels.create({
+                    name: `ticket-${interaction.user.name}`,
+                    type: ChannelType.GuildText,
+                    parent: dbTicket.openTicket,
+                    permissionOverwrites: [
+                        {
+                            id: interaction.member.tag,
+                            allow: [
+                                PermissionsBitField.Flags.ViewChannel,
+                                PermissionsBitField.Flags.SendMessages,
+                                PermissionsBitField.Flags.AttachFiles,
+                            ],
+                        },
+                        {
+                            id: interaction.guild.id,
+                            deny: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: dbTicket.staffId,
+                            allow: [
+                                PermissionsBitField.Flags.ViewChannel,
+                                PermissionsBitField.Flags.SendMessages,
+                                PermissionsBitField.Flags.ManageChannels,
+                            ],
+                        },
+                    ],
+                })]
             }
+        } else if (error) {
+            console.log(error);
+            interaction.reply({ content: 'Sorry, command isn\'t working yet.', ephemeral: true })
         }
     }
 };
