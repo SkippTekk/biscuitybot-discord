65b859cf1e7178b8438d89df4efff4d7e5247692
Mackenzie Molloy <48184419+MackenzieMolloy@users.noreply.github.com>
2023-04-24 21:23:21 +0100

    Update syncCommands.js

--- syncCommands.js
+++ syncCommands.js
@@ -3,36 +3,39 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 
+class CommandProcessor {
+  constructor(client) {
+    return this.main(client);
+  }
 
-function getFiles(dir){
-    const files = fs.readdirSync(dir, {
-        withFileTypes: true
-    });
-    let commandFiles = []
+  async main(client) {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    client.commands = new Collection();
+    let commandFiles = [];
 
-    for(const file of files){
-        if(file.isDirectory()){
-            commandFiles = [
-                ...commandFiles,
-                ...getFiles(`${dir}/${file.name}`),
-            ]
-        } else if(file.name.endsWith('.js')){
-            commandFiles.push(`${dir}/${file.name}`);
-        }
+    for (const file of files) {
+        if (file.isDirectory()) commandFiles = [ ...commandFiles, ...getFiles(`${dir}/${file.name}`), ];
+        else if (file.name.endsWith('.js')) commandFiles.push(`${dir}/${file.name}`);
     }
-    return commandFiles;
-};
 
-let commands = [];
-const commandFiles = getFiles('./commands');
+    let commands = [];
+    const commandFiles = getFiles('./commands');
 
-for(const file of commandFiles){
-    const command = require(file);
-    commands.push(command.data.toJSON());
+    for (const file of commandFiles) {
+        const command = require(file);
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+    }
+
+    const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+
+    rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands})
+    .then(() => console.log(`Commands are sent and recieved.`))
+    .catch(console.error);
+      
+    return client.commands;  
+  }
+
 }
 
-const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
-
-rest.put(Routes.applicationCommands(process.env.CLIENT_ID),{ body: commands})
-.then(() => console.log(`Commands are sent and recieved.`))
-.catch(console.error);
+module.exports = CommandProcessor;
