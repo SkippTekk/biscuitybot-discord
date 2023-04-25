d98f7c8a2fda6ae79ef0b490f157be2067d061b2
SkippTekk <skipptekk@gmail.com>
2023-04-25 13:21:27 -0600

    forgot bot crash prevention

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const path = require('node:path')
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 
+process.on('uncaughtException', (error, source) => {
+    console.log(error);
+
+    client.channels.cache.get(process.env.BOT_ERRORS).send("**Bot break report**: \n \`\`\`javascript \n" + error + "\`\`\`\n\`\`\`" + source + "\`\`\`")
+});
+
 
 const eventsPath = path.join(__dirname, 'events')
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('js'));
