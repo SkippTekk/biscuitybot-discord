6bdf6201ac15cb95828c6e5fedf8ea54a9f41197
skipptekk <skipptekk@gmail.com>
2023-06-17 17:11:43 -0600

    possible fix for console spam

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,15 +3,20 @@ module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
         const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
-        await member.roles.add(dbGuild.defaultRole);
-        const channel = await member.guild.channels.fetch(dbGuild.logChannel)
-        channel.send(`:regional_indicator_m: New member has joined! ${member.user}`)
 
-        if (dbGuild.welcomeChannelId) {
+        if (dbGuild.defaultRole === null) {
+            const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
+            return console.log(dbGuild.guildName)
+
+        } else {
+            const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
+            await member.roles.add(dbGuild.defaultRole);
+            const channel = await member.guild.channels.fetch(dbGuild.logChannel)
+            channel.send(`:regional_indicator_m: New member has joined! ${member.user}`)
             const welcomeChannelId = await member.guild.channels.fetch(dbGuild.welcomeChannelId);
             welcomeChannelId.send(`Welcome to the server ${member.user}, Be sure to check the rules and follow them!`)
-        } else if (dbGuild.welcomeChannelId === null) {
-            return;
         }
+
+
     }
 }
