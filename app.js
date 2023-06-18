06fac147f12dcbc12c4987e9cc1aaaa6687349f8
skipptekk <skipptekk@gmail.com>
2023-06-17 21:16:34 -0600

    added if checks for errors

--- app.js
+++ app.js
@@ -1,136 +1,154 @@
 require("dotenv").config();
-const { Client, GatewayIntentBits, EmbedBuilder, InteractionCollector } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 
-const Guild = require('./models/guild');
-const Ticket = require('./models/ticket');
-
+const Guild = require("./models/guild");
+const Ticket = require("./models/ticket");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildPresences
-    ],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+  ],
 });
 
 client.on("guildCreate", (guild) => {
+  Guild.create({ guildName: guild.name, id: guild.id });
+  Ticket.create({ guildName: guild.name, id: guild.id });
 
-    Guild.create({ guildName: guild.name, id: guild.id, })
-    Ticket.create({ guildName: guild.name, id: guild.id, })
+  client.channels.cache
+    .get(process.env.GUILD_COUNT)
+    .setName(`Guilds - ${client.guilds.cache.size}`);
 
+  const guildCreateEmbed = new EmbedBuilder()
+    .setTitle("A new Guild added me!")
+    .setTimestamp()
+    .setColor("Green")
+    .addFields(
+      {
+        name: `Guilds Name`,
+        value: `${guild.name}`,
+      },
+      {
+        name: "Member count",
+        value: `${guild.memberCount}`,
+      }
+    );
 
-
-    client.channels.cache
-        .get(process.env.GUILD_COUNT)
-        .setName(`Guilds - ${client.guilds.cache.size}`);
-
-    const guildCreateEmbed = new EmbedBuilder()
-        .setTitle("A new Guild added me!")
-        .setTimestamp()
-        .setColor("Green")
-        .addFields(
-            {
-                name: `Guilds Name`,
-                value: `${guild.name}`,
-            },
-            {
-                name: "Member count",
-                value: `${guild.memberCount}`,
-            }
-        );
-
-    client.channels.cache
-        .get(process.env.GUILD_ADD)
-        .send({ embeds: [guildCreateEmbed] });
+  client.channels.cache
+    .get(process.env.GUILD_ADD)
+    .send({ embeds: [guildCreateEmbed] });
 });
 
 client.on("guildDelete", (guild) => {
-    Guild.destroy({ where: { guildName: guild.name } })
-    Ticket.destroy({ where: { guildName: guild.name } })
+  Guild.destroy({ where: { guildName: guild.name } });
+  Ticket.destroy({ where: { guildName: guild.name } });
 
-    client.channels.cache
-        .get(process.env.GUILD_COUNT)
-        .setName(`Guilds - ${client.guilds.cache.size}`);
+  client.channels.cache
+    .get(process.env.GUILD_COUNT)
+    .setName(`Guilds - ${client.guilds.cache.size}`);
 
-    const guildDeleteEmbed = new EmbedBuilder()
-        .setTitle("A guild removed me!")
-        .setTimestamp()
-        .setColor("Red")
-        .addFields(
-            {
-                name: `Guilds Name`,
-                value: `${guild.name}`,
-            },
-            {
-                name: "Member count",
-                value: `${guild.memberCount}`,
-            }
-        );
+  const guildDeleteEmbed = new EmbedBuilder()
+    .setTitle("A guild removed me!")
+    .setTimestamp()
+    .setColor("Red")
+    .addFields(
+      {
+        name: `Guilds Name`,
+        value: `${guild.name}`,
+      },
+      {
+        name: "Member count",
+        value: `${guild.memberCount}`,
+      }
+    );
 
-    client.channels.cache
-        .get(process.env.GUILD_REMOVE)
-        .send({ embeds: [guildDeleteEmbed] });
+  client.channels.cache
+    .get(process.env.GUILD_REMOVE)
+    .send({ embeds: [guildDeleteEmbed] });
 });
 
-client.on('messageDelete', async (message) => {
-    const dbMessageDelete = await Guild.findOne({ where: { id: message.guild.id } });
+client.on("messageDelete", async (message) => {
+  const dbMessageDelete = await Guild.findOne({
+    where: { id: message.guild.id },
+  });
 
-    client.channels.cache.get(dbMessageDelete.logChannel).send({ content: `:regional_indicator_d: Message delete by ${message.author.tag} - \`\`${message.content}\`\`` });
+  if (dbMessageDelete.logChannel === null) {
+    return console.log(dbMessageDelete.guildName + ' - doesn\'t have log set')
+  } else {
 
+    client.channels.cache.get(dbMessageDelete.logChannel).send({
+      content: `:regional_indicator_d: Message delete by ${message.author.tag} - \`\`${message.content}\`\``,
+    });
+  }
 });
 
-client.on('guildMemberRemove', async (member) => {
-    const dbMemberLeave = await Guild.findOne({ where: { id: member.guild.id } });
+client.on("guildMemberRemove", async (member) => {
+  const dbMemberLeave = await Guild.findOne({ where: { id: member.guild.id } });
 
-    client.channels.cache.get(dbMemberLeave.logChannel).send({ content: `:regional_indicator_m::regional_indicator_r: ${member.user} Left.` });
-})
-client.on('messageUpdate', async (messageOld, messageNew) => {
-    if (messageOld.author.bot) return;
-    if (messageOld.content === messageNew.content) return;
+  client.channels.cache.get(dbMemberLeave.logChannel).send({
+    content: `:regional_indicator_m::regional_indicator_r: ${member.user} Left.`,
+  });
+});
+client.on("messageUpdate", async (messageOld, messageNew) => {
+  if (messageOld.author.bot) return;
+  if (messageOld.content === messageNew.content) return;
 
-    const dbMessageUpdate = await Guild.findOne({ where: { id: messageOld.guild.id } })
+  const dbMessageUpdate = await Guild.findOne({
+    where: { id: messageOld.guild.id },
+  });
 
-    client.channels.cache.get(dbMessageUpdate.logChannel).send({ content: `:regional_indicator_e: Message edited. \nOld: \`\`\`${messageOld}\`\`\` New: \`\`\`${messageNew}\`\`\` ` })
+  client.channels.cache.get(dbMessageUpdate.logChannel).send({
+    content: `:regional_indicator_e: Message edited. \nOld: \`\`\`${messageOld}\`\`\` New: \`\`\`${messageNew}\`\`\` `,
+  });
+});
+client.on("channelCreate", async (channel) => {
+  const dbChannelCreate = await Guild.findOne({
+    where: { id: channel.guild.id },
+  });
 
-})
-client.on('channelCreate', async (channel) => {
-    const dbChannelCreate = await Guild.findOne({ where: { id: channel.guild.id } });
+  client.channels.cache.get(dbChannelCreate.logChannel).send({
+    content: `:regional_indicator_c::regional_indicator_m: ${channel.name} was created.`,
+  });
+});
+client.on("channelDelete", async (channel) => {
+  const dbChannelDelete = await Guild.findOne({
+    where: { id: channel.guild.id },
+  });
 
-    client.channels.cache.get(dbChannelCreate.logChannel).send({ content: `:regional_indicator_c::regional_indicator_m: ${channel.name} was created.` });
-
-})
-client.on('channelDelete', async (channel) => {
-    const dbChannelDelete = await Guild.findOne({ where: { id: channel.guild.id } });
-
-    client.channels.cache.get(dbChannelDelete.logChannel).send({ content: `:regional_indicator_c::regional_indicator_d: ${channel.name} was deleted.` });
-
-})
+  client.channels.cache.get(dbChannelDelete.logChannel).send({
+    content: `:regional_indicator_c::regional_indicator_d: ${channel.name} was deleted.`,
+  });
+});
 
 process.on("uncaughtException", (error, source) => {
-    console.log(error);
-    const channel = client.channels.cache.get(process.env.BOT_ERRORS);
+  console.log(error);
+  const channel = client.channels.cache.get(process.env.BOT_ERRORS);
+  console.log(client.guilds.resolve());
 
-    channel.send(`**Bot break report**: \n Server: ${client.guild} \n\`\`\`javascript \n" ${error} "\`\`\`\n\`\`\`"  ${source}  "\`\`\``);
+  channel.send(
+    `**Bot break report**: \n Server: NA \n\`\`\`javascript \n" ${error} "\`\`\`\n\`\`\`"  ${source}  "\`\`\``
+  );
 });
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
-    .readdirSync(eventsPath)
-    .filter((file) => file.endsWith("js"));
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith("js"));
 
 for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
 
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.login(process.env.TOKEN);
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,22 +1,30 @@
-const Guild = require('../models/guild');
+const Guild = require("../models/guild");
 module.exports = {
-    name: 'guildMemberAdd',
-    async execute(member) {
-        const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
+  name: "guildMemberAdd",
+  async execute(member) {
+    const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
 
-        if (dbGuild.defaultRole === null) {
-            const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
-            return console.log(dbGuild.guildName)
+    if (dbGuild.defaultRole === null) {
+      const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
+      return console.log(dbGuild.guildName + " - Has no default Role set.");
+    } else {
+      const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
+      await member.roles.add(dbGuild.defaultRole);
 
-        } else {
-            const dbGuild = await Guild.findOne({ where: { id: member.guild.id } });
-            await member.roles.add(dbGuild.defaultRole);
-            const channel = await member.guild.channels.fetch(dbGuild.logChannel)
-            channel.send(`:regional_indicator_m: New member has joined! ${member.user}`)
-            const welcomeChannelId = await member.guild.channels.fetch(dbGuild.welcomeChannelId);
-            welcomeChannelId.send(`Welcome to the server ${member.user}, Be sure to check the rules and follow them!`)
-        }
-
-
+      if (dbGuild.logChannel === null) {
+        return console.log(dbGuild.guildName + " - has no log channel.");
+      } else {
+        const channel = await member.guild.channels.fetch(dbGuild.logChannel);
+        channel.send(
+          `:regional_indicator_m: New member has joined! ${member.user}`
+        );
+        const welcomeChannelId = await member.guild.channels.fetch(
+          dbGuild.welcomeChannelId
+        );
+        welcomeChannelId.send(
+          `Welcome to the server ${member.user}, Be sure to check the rules and follow them!`
+        );
+      }
     }
-}
+  },
+};
